39a0dfb650232481c753f0cf45e2a88bb0d524ac
amyflo <amyflo@github.com>
2020-11-30 14:38:59 -0800

    Install npm + create js function

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function showResult() {
+  var title = $('title').getValue();
+  var description = $('description').getValue();
+  var ctatext = $('cta-text').getValue();
+  var url = $('url').getValue();
+}
